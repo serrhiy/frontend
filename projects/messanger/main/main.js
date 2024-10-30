0dfab88d745ef5f3ad81a42d6c8fc06523a66506
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-30 22:59:05 +0200

    add message sending

--- projects/messanger/main/main.js
+++ projects/messanger/main/main.js
@@ -1,28 +1,43 @@
 'use strict';
 
-const resizer = document.getElementsByClassName('resizer').item(0);
-const chats = document.getElementsByClassName('chat-list').item(0);
-const dialog = document.getElementsByClassName('dialog').item(0);
+import setResizing from './resizing.js';
 
-const setResizing = () => {
-  let resizing = false;
-  resizer.addEventListener('mousedown', () => {
-    document.body.style.cursor = 'ew-resize';
-    resizing = true;
-  });
-  document.addEventListener('mouseup', () => {
-    document.body.style.cursor = 'auto';
-    resizing = false;
-  });
-  document.addEventListener('mousemove', (event) => {
-    if (!resizing) return;
-    const { innerWidth: width } = window;
-    const { clientX: x } = event;    
-    const chatsPart = 100 * x / width;
-    const dialogsPart = 100 - chatsPart;
-    chats.style.flex = chatsPart;
-    dialog.style.flex = dialogsPart;
-  });
+const messageInput = document.getElementById('message-input');
+const messages = document.getElementsByClassName('messages').item(0);
+
+const getCurrentTime = () => {
+  const date = new Date();
+  const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+  return date.toLocaleString('en-US', options);
+}
+
+const buildMessage = (text, time, myMessage = true) => {
+  const message = document.createElement('div');
+  const messageText = document.createElement('div');
+  const messageTime = document.createElement('div');
+  message.className = myMessage ? 'my-message' : 'persons-message';
+  messageText.className = 'text';
+  messageTime.className = 'time';
+  messageText.appendChild(document.createTextNode(text));
+  messageTime.appendChild(document.createTextNode(time));
+  message.append(messageText, messageTime);
+  return message;
 };
+
+messageInput.addEventListener('keypress', (event) => {
+  if (event.key !== 'Enter') return;
+  const { value: text } = messageInput;
+  const time = getCurrentTime();
+  const message = buildMessage(text, time);
+  messages.prepend(message);
+  messageInput.value = '';
+  setTimeout(() => {
+    const answer = 'My answer is: ' + text;
+    const answerTime = getCurrentTime();
+    const answerMessage = buildMessage(answer, answerTime, false);
+    messages.prepend(answerMessage);
+  }, 3000);
+});
+
 
 setResizing();
---
+++ projects/messanger/main/resizing.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const resizer = document.getElementsByClassName('resizer').item(0);
+const chats = document.getElementsByClassName('chat-list').item(0);
+const dialog = document.getElementsByClassName('dialog').item(0);
+
+export default () => {
+  let resizing = false;
+  resizer.addEventListener('mousedown', () => {
+    document.body.style.cursor = 'ew-resize';
+    resizing = true;
+  });
+  document.addEventListener('mouseup', () => {
+    document.body.style.cursor = 'auto';
+    resizing = false;
+  });
+  document.addEventListener('mousemove', (event) => {
+    if (!resizing) return;
+    const { innerWidth: width } = window;
+    const { clientX: x } = event;    
+    const chatsPart = 100 * x / width;
+    const dialogsPart = 100 - chatsPart;
+    chats.style.flex = chatsPart;
+    dialog.style.flex = dialogsPart;
+  });
+};
