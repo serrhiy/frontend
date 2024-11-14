05d987b4772f8ca3665a70ecaf6a91231864ec04
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-14 05:29:51 +0200

    add sign up page

---
+++ projects/messanger/signup/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fileInput = document.getElementById('file-input');
+const fileInputLable = document.getElementById('file-input-label');
+
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  fileInputLable.innerText = file.name;
+  // fileInputLable.innerText = 'selected';
+})
+
